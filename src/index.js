99c6579dc4c286b2461a532029d315cfab30a40c
Rahul Krishnan <rahul.krishnan@diagnal.com>
2017-10-02 13:43:16 +0530

    temp fix : added CORS headers to all responses

--- src/index.js
+++ src/index.js
@@ -32,6 +32,10 @@ var _ReqRes = function _ReqRes(event, context, callback) {
 
         callback(null, {
             statusCode: statusCode,
+            headers: {
+              "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+              "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+            },
             body: body
         });
     };
